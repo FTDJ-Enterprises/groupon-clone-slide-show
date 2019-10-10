93ca053c573aeb0686aca97450982c78d3b442be
Thomas Johnson <thomasij813@gmail.com>
2019-10-10 14:38:59 -0700

    Configure webpack for es6 and jsx Set up webpack to transpile code from es6 and jsx to js.

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
