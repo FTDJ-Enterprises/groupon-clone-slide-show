a2edf2bb7de5e9881473981034ddba524a873c63
Thomas Johnson <thomasij813@gmail.com>
2019-10-16 20:48:06 -0700

    Incorporate PR Request Changes

--- fakeDataGen.js
+++ fakeDataGen.js
@@ -7,24 +7,15 @@ const outputArray = [];
 // Create 100 records
 const numRecords = 100;
 for (let i = 1; i <= numRecords; i++) {
-  const imageIds = [
-    445,
-    3,
-    225,
-    21,
-    20,
-    201,
-    2,
-    180,
-    160,
-    119
-  ];
 
-  const numOfImages = Math.floor(Math.random() * Math.floor(imageIds.length - 1)) + 1;
+  // Selection of ids from Picsum images for pictures that resemble products
+  const picsumImageIds = [445, 3, 225, 21, 20, 201, 2, 180, 160, 119];
+
+  const numOfImages = Math.floor(Math.random() * (picsumImageIds.length - 1)) + 1;
 
   const imagesArray = [];
   for (let j = 0; j < numOfImages; j++) {
-    imagesArray.push(`http://picsum.photos/id/${imageIds[j]}/650/400`);
+    imagesArray.push(`http://picsum.photos/id/${picsumImageIds[j]}/650/400`);
   }
 
   const record = {
