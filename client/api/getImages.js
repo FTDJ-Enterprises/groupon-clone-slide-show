0e7da60c2ae8d8b1249bb1253db614a995549395
Thomas Johnson <thomasij813@gmail.com>
2019-10-21 12:25:51 -0700

    Prepare Module for Use in Proxy Server

--- client/api/getImages.js
+++ client/api/getImages.js
@@ -2,7 +2,7 @@ export default () => {
   const urlParams = new URLSearchParams(window.location.search);
   const productId = urlParams.get('productId');
 
-  return fetch(`/api/images/${productId}`)
+  return fetch(`http://localhost:3002/api/images/${productId}`)
     .then((res) => res.json())
     .then((data) => data[0].imageUrls)
     .catch((err) => console.error(err));
